19a5282caa8fcf4c5b3603fe07f8abd1958066a2
mr-robot <budimanokky93@gmail.com>
2017-07-14 01:30:39 +0700

    first commit

---
+++ scripts/components/props.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="App-header">
+              <h2>Welcome to React</h2>
+            </div>
+        );
+    }
+}
+
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <p className="App-footer">
+              Ini Footer
+            </p>
+        );
+    }
+}
+
